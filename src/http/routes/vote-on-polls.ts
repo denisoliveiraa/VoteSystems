60204a9c7cb33e710d54db71ed64442ce2af0059
denisoliveiraa <denis_o@hotmail.com.br>
2024-02-11 11:31:08 -0300

    adding unique value foruser to ensure unique vote

--- src/http/routes/vote-on-polls.ts
+++ src/http/routes/vote-on-polls.ts
@@ -1,8 +1,9 @@
 import {z} from 'zod'
+import {randomUUID} from "node:crypto"
 import {prisma} from "../../lib/prisma"
 import { FastifyInstance } from "fastify"
 
-export async function createPoll(app: FastifyInstance){
+export async function voteOnPoll(app: FastifyInstance){
 app.post('/polls/:pollId/votes', async (request, reply) => {
   const voteOnPollBody = z.object({
     pollIdOptionId: z.string().uuid()
@@ -15,6 +16,18 @@ app.post('/polls/:pollId/votes', async (request, reply) => {
   const {pollId} = VoteOnPollParams.parse(request.params)
   const {pollIdOptionId} = voteOnPollBody.parse(request.body)
 
+  let { sessionId } = request.cookies
+
+  if(!sessionId) {
+    sessionId = randomUUID()
+
+    reply.setCookie('sessionId', sessionId, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 30,
+      signed: true,
+      httpOnly: true,
+    })
+  }
   return reply.status(201).send()
 })
 }
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,20 @@
 import fastify from 'fastify'
 import {createPoll} from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-polls'
+import  cookie from '@fastify/cookie'
 
 const app = fastify()
 
+app.register( cookie, {
+  secret: "polls-app", 
+  hook: 'onRequest', 
+  parseOptions: {}  
+})
+
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({port: 3333}).then(() => {
   console.log('HTTP server running')
